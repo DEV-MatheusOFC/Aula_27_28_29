fcfccd3b8818af164de82383a217049bf459b008
DEV-MatheusOFC <matheusnunesoliveira198@gmail.com>
2020-06-29 10:32:10 -0300

    Aula 27 Finalizada

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Aula_27_28_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p1 = new Produto();
+            p1.Codigo = 1;
+            p1.Nome = "Sasmsumg Galaxy A20";
+            p1.Preco = 800f;
+
+            p1.Cadastrar(p1);
+        }
+    }
+}
